a80214a4e5c1c5d16647eb2b408f2196802a49f3
Pamela Chong <pamelachong@Pamelas-MacBook-Air.local>
2018-07-19 16:02:17 -0700

    Implemented final form with some validation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,140 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Form, Field } from "react-final-form";
+
+
+
+const onSubmit = values => {
+  document.getElementById("texthere").innerHTML='';
+  console.log(values);
+  document.getElementById("texthere").innerHTML+= `<h1>Topic: ${values.topic} </h1> <p>StringStuff: ${values.sampleField} </p><p>by ${values.firstName} ${values.lastName} </p>`
+
+};
+
+const required = value => (value ? undefined : "Required");
+const isString = value => ( 
+  typeof value === 'string'? true : 'Needs text'
+);
+
 
 class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     value: "Sad times"
+  //   };
+  //   // this.handleChange = this.handleChange.bind(this);
+  //   // this.inputRef = React.createRef();
+  //   // this.handleSubmit = this.handleSubmit.bind(this);
+  // }
+
+  // handleChange(event) {
+  //   this.setState({ value: event.target.value });
+  // }
+ validate = values =>{
+   console.log(values)
+ }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Form
+          onSubmit={onSubmit}
+           validate={this.validate}
+
+          render={({ handleSubmit, pristine, invalid }) => (
+            <form onSubmit={handleSubmit}>
+              {/* <Field
+              validate={required}
+                render={({ input, meta }) => (
+                  <div> */}
+                    {/* <label>Bio</label>
+                    <textarea {...input} />
+                    {meta.touched && meta.error && <span>{meta.error}</span>} */}
+
+                    {/* <label htmlFor="controlled">
+                      Controlled Input:
+                      <input
+                        name="controlled"
+                        type="text"
+                        value={this.state.value}
+                        onChange={event => this.handleChange(event)}
+                      />
+                    </label> */}
+                    <div>
+                      <label>Topic Title</label>
+                      <Field
+                        name="topic"
+                        component="input"
+                        type="text"
+                        placeholder="Topic Title"
+                        validate={required}
+                        render={({ input, meta }) => (
+                          <div>
+                            <input {...input} />
+                          </div>
+                          // {meta.touched && meta.error && <span>{meta.error}</span>}
+                        )}
+                      />
+                    </div>
+                    <div>
+                      <Field
+                        name="sampleField"
+                        component="input"
+                         >
+                        {({input, meta})=>(
+                          <div>
+                            <label>Write something</label>
+                          <input
+                            {...input}
+                            type="text"
+                            placeholder="Text here"
+                          />
+                          {meta.error &&
+                            meta.touched && <span>{meta.error}</span>}
+                        </div>
+                        )}
+                      </Field>
+                    </div>
+
+                    <Field name="lastName" validate={required}>
+                      {({ input, meta }) => (
+                        <div>
+                          <label>Last Name</label>
+                          <input
+                            {...input}
+                            type="text"
+                            placeholder="Last Name"
+                          />
+                          {meta.error &&
+                            meta.touched && <span>{meta.error}</span>}
+                        </div>
+                      )}
+                    </Field>
+
+                     <Field name="firstName" validate={required}>
+                      {({ input, meta }) => (
+                        <div>
+                          <label>First Name</label>
+                          <input
+                            {...input}
+                            type="text"
+                            placeholder="First Name"
+                          />
+                          {meta.error &&
+                            meta.touched && <span>{meta.error}</span>}
+                        </div>
+                      )}
+                    </Field>
+
+                    <button type="submit">Submit Pls</button>
+                  {/* </div>
+                )}
+              /> */}
+            </form>
+          )}
+        />
+        <div id='texthere'></div>
       </div>
     );
   }
